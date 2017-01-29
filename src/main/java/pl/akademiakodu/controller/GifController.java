b4ede3006de10085329e8f7e592d218a19f71549
Rafał Dudziak <dudziak.rafal@gmail.com>
2017-01-29 11:18:15 +0100

    Dodano gif-details  wyswietla szczegoly gifa

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -2,14 +2,23 @@ package pl.akademiakodu.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.akademiakodu.model.Gif;
 
 @Controller
 public class GifController {
 
     @GetMapping("/")
-    public String listGifs(){
+    public String listGifs() {
         return "home";
+    }
+
+    @GetMapping("/gif")
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", "Rav", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 
 }
--- src/main/java/pl/akademiakodu/model/Gif.java
+++ src/main/java/pl/akademiakodu/model/Gif.java
@@ -1,14 +1,40 @@
 package pl.akademiakodu.model;
 
-/**
- * Created by gosc on 29.01.2017.
- */
 public class Gif {
     private String name;
     private String username;
-    private boolean favourite;
 
-    public String getName(){
-     return name;
+    private boolean favorite;
+
+    public Gif(String name, String username, boolean favourite) {
+        this.name = name;
+        this.username = username;
+        this.favorite = favourite;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
     }
 }
+
