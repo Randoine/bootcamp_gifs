70924716240801ac5b47814ba84afbedf70e8bf9
Rafał Dudziak <dudziak.rafal@gmail.com>
2017-01-29 11:03:10 +0100

    Work in progress

---
+++ src/main/java/pl/akademiakodu/model/Gif.java
@@ -0,0 +1,14 @@
+package pl.akademiakodu.model;
+
+/**
+ * Created by gosc on 29.01.2017.
+ */
+public class Gif {
+    private String name;
+    private String username;
+    private boolean favourite;
+
+    public String getName(){
+     return name;
+    }
+}
